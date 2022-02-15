0844e90ec5c58a481ee710fe61d1e5216dba7911
ambleyva <ambleyva24@gmail.com>
2022-02-14 20:39:23 -0500

    idk

--- assets/script.js
+++ assets/script.js
@@ -10,7 +10,7 @@ let sugar = 0 // Sugar Sprinkle
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
 // selecting the element with an id of add-gb
-const gbPlusBtn= document.querySelector('#add-gb')
+const gbPlusBtn = document.querySelector('#add-gb')
 
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
@@ -23,6 +23,6 @@ let gbPlusVotesCell=document.getElementById ("qty-gb").textContent;
 gbPlusBtn.addEventListener('click', function(e) {
     let gbPlusVotesCount = parseInt(gbPlusVotesCell.textContent);
     gbPlusVotesCount = gbPlusVotesCount + 1;
-    gbPlusVotesCell.textContent = gbPlusVotesCount
+    gbPlusVotesCell.textContent = gbPlusVotesCount;
 });
 console.log('Gingerbread + button was clicked!')
