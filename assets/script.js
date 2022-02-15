183cddc395b1dfbfcb2d4b55687a23c6bbe249cd
ambleyva <ambleyva24@gmail.com>
2022-02-14 20:21:15 -0500

    0

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,7 @@ credit.textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 
-let gbPlusVotesCell=document.getElementsByTagName("qty-gb")[0].textContent;
+let gbPlusVotesCell=document.getElementsByTagName("qty-gb").textContent;
 
 
 gbPlusBtn.addEventListener('click', function(e) {
