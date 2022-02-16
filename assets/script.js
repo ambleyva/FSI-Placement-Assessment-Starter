430e35ce0c7d97b6429361496f3ad1d3bc37d1d5
ambleyva <ambleyva24@gmail.com>
2022-02-16 18:19:03 -0500

    const all cookies

--- assets/script.js
+++ assets/script.js
@@ -9,18 +9,30 @@ let sugar = 0 // Sugar Sprinkle
 
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
 
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
+// selecting the element with an id of add-gb
+const gbPlusBtn = document.querySelector('#add-gb')
+const gbMinusBtn = document.querySelector('#minus-gb')
+
+const ccPlusBtn = document.querySelector('#add-cc')
+const ccMinusBtn = document.querySelector('#minus-cc')
+
+const scPlusBtn = document.querySelector('#add-sugar')
+const scMinusBtn = document.querySelector('#Minus-sugar')
+
+//Attach Tally Count to Qty 
+
+
 // Event listener for clicks on the "+" button for Gingerbread cookies
 
-let gbPlusVotesCell=document.getElementById ("qty-gb").textContent;
+let gbPlusVotesCell = document.getElementById("qty-gb").textContent;
 
 
-gbPlusBtn.addEventListener('click', function(_e) {
+
+gbPlusBtn.addEventListener('click', function( ) {
     let gbPlusVotesCount = parseInt(gbPlusVotesCell.textContent);
     gbPlusVotesCount = gbPlusVotesCount + 1;
     gbPlusVotesCell.textContent = gbPlusVotesCount;
