80c1d76205ee7465f8b9bb33f0c87fa994c9a4dd
ambleyva <ambleyva24@gmail.com>
2022-02-16 19:29:42 -0500

    +=

--- assets/script.js
+++ assets/script.js
@@ -34,7 +34,7 @@ const ctTallyCount = document.querySelector('#qty-total').textContent
 // Event listener for clicks on the "+" button for Gingerbread cookies
 const count = 0;
 gbPlusBtn.addEventListener('click', function(e) {
-    count +=1;
+    count += 1;
     gbCounter.textContent = count;
 });
 
